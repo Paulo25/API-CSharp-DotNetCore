c7e33d9b3f64072128d2b16e26a48b6a7bd68168
Paulo Vitor <paulo.vitor.cs97@gmail.com>
2024-03-19 18:45:27 -0300

    feat: first deploy

---
+++ Services/AuthenticateService.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace AlunosApi.Services
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManeger;
+
+        public AuthenticateService(SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManeger)
+        {
+            _signInManager = signInManager;
+            _userManeger = userManeger;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false,
+                lockoutOnFailure: false);
+
+                return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var appUser = new IdentityUser
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            var result = await _userManeger.CreateAsync(appUser, password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(appUser, isPersistent: false);
+            }
+
+            return result.Succeeded;
+        }
+    }
+}
---
+++ Services/IAlunoService.cs
@@ -0,0 +1,16 @@
+﻿using AlunosApi.Models;
+
+namespace AlunosApi.Services
+{
+    public interface IAlunoService
+    {
+        Task<IEnumerable<Aluno>> GetAlunos();
+        Task<IEnumerable<Aluno>> GetAlunosByNome(string nome);
+        Task<Aluno> GetAluno(int id); 
+        Task CreateAluno(Aluno aluno);
+        Task UpdateAluno(Aluno aluno);
+        Task DeleteAluno(Aluno aluno);
+        
+
+    }
+}
